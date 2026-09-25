b6963d6fa1d82f25732f6371e4ff40466ff1435e
Vaju <235863328+vajashiodedara56-netizen@users.noreply.github.com>
2026-09-25 22:44:54 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -537,7 +537,7 @@ KEYRING_FILE  = DIRS["data"] / "keyring.json"   # tiny local cache only
 # ┌──────────────────────────────────────────────────────────────┐
 # │  BOT TOKEN  add karo.   ││
 # └──────────────────────────────────────────────────────────────┘
-BOT_TOKEN_HARDCODED = "BOT_TOKEN"   # ← ADD BOT TOKEN
+BOT_TOKEN_HARDCODED = "8842724455:AAE0vaUtV03afd7IMTDDxtftD0DrWkUYKps"   # ← ADD BOT TOKEN
 TOKEN = (
     os.environ.get("BOT_TOKEN")
     or os.environ.get("MAIN_BOT_TOKEN")
@@ -546,7 +546,7 @@ TOKEN = (
     or ""
 ).strip()
 try:
-    OWNER_ID = int(os.environ.get("OWNER_ID", "8253072984"))
+    OWNER_ID = int(os.environ.get("OWNER_ID", "8316296308"))
 except (TypeError, ValueError):
     OWNER_ID = 0
 if not TOKEN:
